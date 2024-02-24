f8f56fb805e00e20faf171cffe16ad3e57866462
cetobi <andre.marcdias@gmail.com>
2024-02-23 22:18:11 -0300

    feat: changed home screen styles

--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,5 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
 });
