c62510e7aa8c0a70293af5b8317109ca97c937b2
cetobi <andre.marcdias@gmail.com>
2024-02-26 20:48:56 -0300

    feat: created forecast function and add assets

--- src/API/OpenWeather/index.ts
+++ src/API/OpenWeather/index.ts
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
+const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?appid=${process.env.EXPO_PUBLIC_OPENWEATHER_API}&units=metric&lang=pt_br`
 const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.EXPO_PUBLIC_OPENWEATHER_API}&units=metric&lang=pt_br`
 const geocodingUrl = `http://api.openweathermap.org/geo/1.0/direct?appid=${process.env.EXPO_PUBLIC_OPENWEATHER_API}&limit=3`
 
-export async function openweather(lat: number, lon: number) {
+export async function getCurrentWeather(lat: number, lon: number) {
     try {
         const response = await axios.get(`${weatherUrl}&lat=${lat}&lon=${lon}`)
         return response.data
@@ -18,5 +19,14 @@ export async function geocoding(location: string) {
         return response.data[0]
     } catch (error) {
         console.log('Error geocoding: ' + error)
+    }
+}
+
+export async function forecast(lat: number, lon: number) {
+    try {
+        const response = await axios.get(`${forecastUrl}&lat=${lat}&lon=${lon}`)
+        return response.data
+    } catch (error) {
+        console.log('Error forecast:'+ error)
     }
 }
