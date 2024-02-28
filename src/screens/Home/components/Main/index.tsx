3f7a8b4fcb7edcaeec307aa1743d7fceec9145be
cetobi <andre.marcdias@gmail.com>
2024-02-27 22:56:40 -0300

    feat: updated Main component, added temp max and min

--- src/screens/Home/components/Main/index.tsx
+++ src/screens/Home/components/Main/index.tsx
@@ -5,15 +5,27 @@ import { styles } from './styles';
 
 interface Props {
     temp: number,
+    temp_max: number,
+    temp_min: number,
     description: string
 }
 
 export function Main(props: Props) {
     return (
-        <View>
-            <Text>Imagem</Text>
-            <Text>{Math.floor(props.temp)}º</Text>
-            <Text>{props.description}</Text>
+        <View style={styles.container}>
+            <View style={styles.image}>
+                <Text>Imagem</Text>
+            </View>
+            <View style={styles.containerTemp}>
+                <View style={styles.mainTemp}>
+                    <Text>{props.description}</Text>
+                    <Text>{Math.floor(props.temp)}º</Text>
+                </View>
+                <View style={styles.maxminTemp}>
+                    <Text>{Math.floor(props.temp_max)}º/</Text>
+                    <Text>{Math.floor(props.temp_min)}º</Text>
+                </View>
+            </View>
         </View>
     );
 }
--- src/screens/Home/components/Main/styles.tsx
+++ src/screens/Home/components/Main/styles.tsx
@@ -2,6 +2,30 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    width: '100%',
+    height: '50%',
+    borderWidth: 1,
   },
+  image: {
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    height: '70%',
+  },
+  containerTemp: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:'space-around',
+    width: '100%',
+    height: '30%',
+  },
+  mainTemp: {
+    borderWidth: 1
+  },
+  maxminTemp: {
+    flexDirection: 'row'
+  }
 });
